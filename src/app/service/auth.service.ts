5edbc75346a1d79ce21cf714a9f01d1d6e4ae9e0
Evelyn Rosa <evelynnunesrosa@gmail.com>
2022-02-24 17:23:43 -0300

    Componente inicio

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -15,11 +16,18 @@ export class AuthService {
 
   entrar (userLogin:UserLogin): Observable<UserLogin> {
     return this.http.post<UserLogin>('https://blog-do-totoro.herokuapp.com/usuarios/login', userLogin)
-
   }
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('https://blog-do-totoro.herokuapp.com/usuarios/cadastro', user)
+  }
 
+  logado(){
+    let ok = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+    return ok
   }
 }
